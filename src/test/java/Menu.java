365c3f457177d5466651922ba739a85a53e5f8bf
yaozhendong <yaozhendong@hualala.com>
2020-12-16 11:14:32 +0800

    feat:add 测试功能目录

---
+++ src/test/java/Menu.java
@@ -0,0 +1,15 @@
+/**
+ * 测试功能目录
+ * @see com.GrpcUtil
+ * {@link com.GrpcUtil}
+ * 推荐使用@link，因为@link可以放在任何地方
+ * @Author: yaozh
+ * @Description:
+ */
+public enum Menu {
+    /**
+     * TOKEN认证测试
+     * {@link org.nuhara.demos.AuthGrpcHeaderTest#oneCallTest()}
+     */
+    AUTH_CLIENT_TEST
+}
